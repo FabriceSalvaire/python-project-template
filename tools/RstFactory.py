a24bc09469db95dcb38b9224f6e271ca5f8c9b4b
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2013-05-10 20:48:47 +0200

    Copyright

--- tools/RstFactory.py
+++ tools/RstFactory.py
@@ -1,7 +1,7 @@
 ####################################################################################################
 # 
 # @Project@ - @ProjectDescription@.
-# Copyright (C) 2012 Salvaire Fabrice
+# Copyright (C) 2012 Fabrice Salvaire
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
